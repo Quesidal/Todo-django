a4baca20faf5771cc66008af28da90b6cd252943
quesidal <alexandrslobodyanik1>
2019-08-08 18:53:33 +0300

    configurate register system add signin, login, logout page add redirect to login page when user isn't login add task filtration for authenticated user

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
-from .views import MainView, TaskList
+from django.urls import path, include
+from .views import TaskList, SignInView
 
 urlpatterns = [
     path('', TaskList.as_view(), name='main page'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signin', SignInView.as_view(), name='sign in'),
+    path('task/crate', ),
+
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,26 @@
-from django.views.generic import TemplateView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView, ListView, CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+
 from .models import Task
 
 
-class MainView(TemplateView):
+class SignInView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signin.html'
+
+
+class LoginView(TemplateView):
+    template_name = "registration/login.html"
+
+
+class TaskList(LoginRequiredMixin, ListView):
+    login_url = 'accounts/login/'
+    model = Task
     template_name = "tasks/task_list.html"
 
-
-class TaskList(ListView):
-    model = Task
-
-    def get(self, request, *args, **kwargs):
-        pass
+    def get_queryset(self):
+        """Filter only user's tasks"""
+        return Task.objects.filter(author=self.request.user)
