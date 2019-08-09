2852bace7585f9eca9e92eff5c3f9f143c73191f
quesidal <alexandrslobodyanik1>
2019-08-09 21:02:22 +0300

    add saving task from form

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
-from .views import TaskList, SignInView
+from .views import TaskList, SignInView, TaskAddView
 
 urlpatterns = [
     path('', TaskList.as_view(), name='main page'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signin', SignInView.as_view(), name='sign in'),
-    path('task/crate', ),
 
+    path('task/add', TaskAddView.as_view(), name='task add'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,15 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
 from django.views.generic import TemplateView, ListView, CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 
-from .models import Task
+from tasks.forms import TaskForm
+from .models import Task, Project
+
+
+class TestView(TemplateView):
+    template_name = 'tasks/main_page.html'
 
 
 class SignInView(CreateView):
@@ -19,8 +25,30 @@ class LoginView(TemplateView):
 class TaskList(LoginRequiredMixin, ListView):
     login_url = 'accounts/login/'
     model = Task
-    template_name = "tasks/task_list.html"
+    template_name = "tasks/main_page.html"
 
-    def get_queryset(self):
-        """Filter only user's tasks"""
-        return Task.objects.filter(author=self.request.user)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['projects'] = Project.objects.filter(author=self.request.user)
+        context['tasks'] = Task.objects.filter(author=self.request.user)
+        context['form'] = TaskForm()
+        return context
+
+
+class TaskAddView(TemplateView):
+    template_name = "tasks/main_page.html"
+
+    def post(self, request, *args, **kwargs):
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            self.__save_task_from_form(form)
+        return redirect('/')
+
+    def __save_task_from_form(self, form):
+        new_task = Task()
+        new_task.author = self.request.user
+        new_task.text = form.cleaned_data['text']
+        new_task.end_time = form.cleaned_data['end_time']
+        new_task.priority = form.cleaned_data['priority']
+        new_task.project = Project.objects.get(name=form.cleaned_data['project'])
+        new_task.save()
